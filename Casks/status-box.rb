89e49611ad7c23ff357c21c193e5f61e2676dde8
Elixir <elixirevo@gmail.com>
2026-06-07 17:57:05 +0900

    Update status-box to 1.1.0

--- Casks/status-box.rb
+++ Casks/status-box.rb
@@ -1,19 +1,20 @@
 cask "status-box" do
-  version "1.0.0"
-  sha256 "c380abf1ab21f67b7ab2c583903d021608f99b399301dc03f41119ca563401d5"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://github.com/elixirevo/statusbox/releases/download/v#{version}/StatusBox-#{version}-universal.dmg"
+  version "1.1.0"
+  sha256 arm:   "2332c5b5a73bf808f1a59c4119c65437f09666afe6c78a8a9d9b42249fbff72a",
+         intel: "b6caf31a3040c33a381ad0b2201f9cb8f91741858c9375849425ef815c338146"
+
+  url "https://github.com/elixirevo/statusbox/releases/download/v#{version}/StatusBox-#{version}-#{arch}.dmg"
   name "Status Box"
-  desc "Native macOS menu bar utility for hiding and opening status bar apps"
+  desc "Menu bar utility for hiding and opening status bar apps"
   homepage "https://github.com/elixirevo/statusbox"
 
-  depends_on macos: ">= :ventura"
+  depends_on macos: :ventura
 
   app "StatusBox.app"
 
   uninstall quit: "com.elixirevo.StatusBox"
 
-  zap trash: [
-    "~/Library/Preferences/com.elixirevo.StatusBox.plist",
-  ]
+  zap trash: "~/Library/Preferences/com.elixirevo.StatusBox.plist"
 end
