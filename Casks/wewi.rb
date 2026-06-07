1b93a17e2a36cb855afc9248072f8bd28541c17a
Elixir <elixirevo@gmail.com>
2026-06-07 17:02:20 +0900

    Update wewi cask to 1.1.1

--- Casks/wewi.rb
+++ Casks/wewi.rb
@@ -1,11 +1,16 @@
 cask "wewi" do
-  version "1.0.2"
-  sha256 "64576c7fb2b01d08c4aa075c17b3d9273a10ae2236c26e2c274c7a40857ddf7a"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://github.com/elixirevo/wewi/releases/download/v#{version}/wewi-#{version}-universal.dmg"
+  version "1.1.1"
+  sha256 arm:   "ab59ac08d7d4b0eceb17c44d5902bba0640771ef8048158479802baaff60e7ef",
+         intel: "9e4082a4ec208af2cdce44bf3aadfa125562b5988f2fc0d8404b43621010060e"
+
+  url "https://github.com/elixirevo/wewi/releases/download/v#{version}/wewi-#{version}-#{arch}.dmg"
   name "wewi"
-  desc "Pin live web pages to your macOS desktop as widgets"
+  desc "Pin live web pages to your desktop as widgets"
   homepage "https://github.com/elixirevo/wewi"
+
+  depends_on macos: ">= :ventura"
 
   app "wewi.app"
 
